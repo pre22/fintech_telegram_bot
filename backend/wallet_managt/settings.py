a96e72bd51783e62688f0b9edff228501b0c7ffb
Precious Chika <43546663+pre22@users.noreply.github.com>
2025-08-01 14:19:17 +0100

    update: added apps to installed_apps

--- backend/wallet_managt/settings.py
+++ backend/wallet_managt/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'wallets.apps.WalletsConfig',
+    'transactions.apps.TransactionsConfig',
 ]
 
 MIDDLEWARE = [
